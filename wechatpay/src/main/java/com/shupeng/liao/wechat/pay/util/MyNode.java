52e865e6b9d4d43c82dab3a0f6ca4eaf260257ef
shupeng.liao <shupeng.liao@baozun.cn>
2017-07-25 11:45:49 +0800

    删除

--- wechatpay/src/main/java/com/shupeng/liao/wechat/pay/util/MyNode.java
+++
@@ -1,9 +0,0 @@
-package com.shupeng.liao.wechat.pay.util;
-
-public class MyNode {
-
-    public static void main(String[] args) {
-	String aString = "a";
-	System.out.println(Float.isNaN(0));
-    }
-}
